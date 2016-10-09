78179b1aa688f0ea750f87a124fb7717b6fba05c
Stéphan Gaud <stephgoeseast@gmail.com>
2016-10-09 21:00:16 +0100

    add photo upload feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:name, :description)
+    params.require(:post).permit(:name, :description, :image)
   end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.jpg"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
